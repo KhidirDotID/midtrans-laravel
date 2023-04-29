6c1482049ed76c68d5f450add5dd095cfa6a08fb
Khidir Zahid <khidirdotid@gmail.com>
2023-04-30 06:59:26 +0700

    .

--- src/Facades/Midtrans.php
+++ src/Facades/Midtrans.php
@@ -4,6 +4,18 @@ namespace KhidirDotID\Midtrans\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \KhidirDotID\Midtrans getSnapToken($params)
+ * @method static \KhidirDotID\Midtrans createTransaction($params)
+ * @method static \KhidirDotID\Midtrans charge($params)
+ * @method static \KhidirDotID\Midtrans capture($param)
+ * @method static \KhidirDotID\Midtrans status($id)
+ * @method static \KhidirDotID\Midtrans approve($id)
+ * @method static \KhidirDotID\Midtrans cancel($id)
+ * @method static \KhidirDotID\Midtrans expire($id)
+ * @method static \KhidirDotID\Midtrans refund($id)
+ * @method static \KhidirDotID\Midtrans deny($id)
+ */
 class Midtrans extends Facade
 {
     /**
